b0de76f1aab97e7a9e84b5ee9eab5169ffc41218
Xavier Cambar <xcambar@gmail.com>
2025-12-19 21:55:07 +0100

    enables chordal hold and permissive hold

---
+++ keyboards/kaly/kaly42/keymaps/xavier/config.h
@@ -0,0 +1,14 @@
+// Copyright 2023 Kael Soares Augusto (@Dwctor)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Tapping term for mod-tap keys (in milliseconds)
+#undef TAPPING_TERM
+#define TAPPING_TERM 250
+
+// Enable permissive hold for better mod-tap behavior
+#define PERMISSIVE_HOLD
+
+// Enable chordal hold for home row mods
+#define CHORDAL_HOLD
