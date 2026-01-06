41fc933eedc93faeb17503b347c7302cbcf06cc5
Xavier Cambar <xcambar@gmail.com>
2026-01-06 18:16:49 +0100

    adds comfort options and disable autoshift

--- keyboards/kaly/kaly42/keymaps/xavier/config.h
+++ keyboards/kaly/kaly42/keymaps/xavier/config.h
@@ -9,7 +9,18 @@
 
 // Enable permissive hold for better mod-tap behavior
 #define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 
 // Enable chordal hold for home row mods
 #define CHORDAL_HOLD
-#define AUTO_SHIFT_TIMEOUT 200
+// #define AUTO_SHIFT_TIMEOUT 200
+
+// One-shot modifier timeout (in ms) - keeps OSM active after layer release
+#define ONESHOT_TIMEOUT 3000
+
+// Retro tapping - tap layer key when released without pressing other keys
+#define RETRO_TAPPING
+#define RETRO_TAPPING_PER_KEY
+
+// Tapping force hold - prevents accidental taps when rolling
+#define TAPPING_FORCE_HOLD_PER_KEY
