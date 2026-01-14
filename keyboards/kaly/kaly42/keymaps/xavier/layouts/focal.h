431477caef2a17eec9f1b9e20fea9895b2fc763d
Xavier Cambar <xcambar@gmail.com>
2026-01-14 22:24:08 +0100

    fixed the FOCAL layout

--- keyboards/kaly/kaly42/keymaps/xavier/layouts/focal.h
+++ keyboards/kaly/kaly42/keymaps/xavier/layouts/focal.h
@@ -1,15 +1,16 @@
 // FOCAL Layout Key Definitions
 // This file maps abstract key positions (_XX_) to actual FOCAL keycodes
 // FOCAL is an optimized layout designed for English typing efficiency
+// Source: https://github.com/Keyhabit/Focal-keyboard-layout
 
 /*
  * FOCAL Layout - Actual Keycodes
  * ┌─────┬─────┬─────┬─────┬─────┬─────┐       ┌─────┬─────┬─────┬─────┬─────┬─────┐
- * │     │  B  │  L  │  D  │  W  │  Z  │       │  '  │  F  │  O  │  U  │  J  │     │
+ * │     │  V  │  L  │  H  │  G  │  K  │       │  Q  │  F  │  O  │  U  │  J  │     │
  * ├─────┼─────┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┼─────┤
- * │     │  N  │  R  │  T  │  S  │  G  │       │  Y  │  H  │  A  │  E  │  I  │  ;  │
+ * │     │  S  │  R  │  N  │  T  │  B  │       │  Y  │  C  │  A  │  E  │  I  │  /  │
  * ├─────┼─────┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┼─────┤
- * │     │  Q  │  X  │  M  │  C  │  V  │       │  K  │  P  │  ,  │  .  │  -  │     │
+ * │     │  Z  │  X  │  M  │  D  │  P  │       │  '  │  W  │  .  │  ;  │  ,  │     │
  * └─────┴─────┴─────┴─────┴─────┴─────┘       └─────┴─────┴─────┴─────┴─────┴─────┘
  *                     ┌─────┐                           ┌─────┐
  *                     │     ├─────┐               ┌─────┤     │
@@ -20,12 +21,12 @@
 
 // Row 0 (top row, columns 0-11)
 #define _00_ KC_NO
-#define _01_ KC_B
+#define _01_ KC_V
 #define _02_ KC_L
-#define _03_ KC_D
-#define _04_ KC_W
-#define _05_ KC_Z
-#define _06_ KC_QUOT
+#define _03_ KC_H
+#define _04_ KC_G
+#define _05_ KC_K
+#define _06_ KC_Q
 #define _07_ KC_F
 #define _08_ KC_O
 #define _09_ KC_U
@@ -34,30 +35,30 @@
 
 // Row 1 (home row, columns 12-23)
 #define _12_ KC_NO
-#define _13_ KC_N
+#define _13_ KC_S
 #define _14_ KC_R
-#define _15_ KC_T
-#define _16_ KC_S
-#define _17_ KC_G
+#define _15_ KC_N
+#define _16_ KC_T
+#define _17_ KC_B
 #define _18_ KC_Y
-#define _19_ KC_H
+#define _19_ KC_C
 #define _20_ KC_A
 #define _21_ KC_E
 #define _22_ KC_I
-#define _23_ KC_SCLN
+#define _23_ KC_SLSH
 
 // Row 2 (bottom row, columns 24-35)
 #define _24_ KC_NO
-#define _25_ KC_Q
+#define _25_ KC_Z
 #define _26_ KC_X
 #define _27_ KC_M
-#define _28_ KC_C
-#define _29_ KC_V
-#define _30_ KC_K
-#define _31_ KC_P
-#define _32_ KC_COMM
-#define _33_ KC_DOT
-#define _34_ KC_MINS
+#define _28_ KC_D
+#define _29_ KC_P
+#define _30_ KC_QUOT
+#define _31_ KC_W
+#define _32_ KC_DOT
+#define _33_ KC_SCLN
+#define _34_ KC_COMM
 #define _35_ KC_NO
 
 // Thumb cluster (keys 36-41)
