7756e2b6e30f95742d80c64ea3cb50732b716cb4
Xavier Cambar <xcambar@gmail.com>
2026-03-21 08:42:36 +0100

    Fixes the NAV toggle behavior

--- keyboards/kaly/kaly42/keymaps/xavier/keymap.c
+++ keyboards/kaly/kaly42/keymaps/xavier/keymap.c
@@ -192,7 +192,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_ESC,  OS_ALT,  OS_GUI,  OS_CTRL, MM_GUICTRL, SW_WIN,                          KC_NO,   KC_NO,   KC_ENT,  KC_NO,   KC_NO,   KC_DEL,
         KC_NO,   SK_UNDO, SK_CUT,  SK_COPY, SK_PSTE, KC_NO,                              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                            KC_NO,   OS_SHFT, KC_NO,                  TG(NAV), OS_SHFT, KC_NO
+                                            KC_NO,   OS_SHFT, KC_NO,                  TO(NAV), OS_SHFT, KC_NO
     ),
      /*
       * Navigation Layer - Arrow keys and navigation
