62c7272169c41e854855794090aa751b4c466f53
Xavier Cambar <xcambar@gmail.com>
2026-04-07 07:44:36 +0200

    feat: add missing keyboard definition to the userspace

---
+++ keyboards/42keebs/cantor_pro/config.h
@@ -0,0 +1,10 @@
+// Copyright 2022 Diego Palacios (@diepala)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
+
+#define SPLIT_HAND_PIN GP19
